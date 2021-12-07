674eb05514542cc24ad79b8d1cfa3051dd8bf778
Dorian Amouroux <dorian@sketch.com>
2021-12-07 09:46:21 +0100

    day 7

---
+++ day7/day7_part_1.ex
@@ -0,0 +1,30 @@
+defmodule Day7 do
+
+  def main() do
+    positions = read_file()
+
+    {min_pos, max_pos} = Enum.min_max(positions)
+
+    min_pos..max_pos
+    |> Enum.map(& get_fuel(&1, positions))
+    |> Enum.min()
+    |> IO.inspect
+  end
+
+  def get_fuel(pos, positions) do
+    positions
+    |> Enum.map(& abs(&1 - pos))
+    |> Enum.sum()
+  end
+
+  defp read_file() do
+    [filename] = System.argv()
+    filename
+    |> File.read!()
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
+Day7.main()
