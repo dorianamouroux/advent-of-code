7baeec217b7583fba8a0941bca3fefbe503dffc8
Dorian Amouroux <dor.amouroux@gmail.com>
2022-09-02 23:56:30 +0200

    add a Map function, using generics

---
+++ 2020/src/utils/slices.go
@@ -0,0 +1,23 @@
+package utils
+
+func Filter[Item interface{}](list []Item, fn func(Item)bool) []Item {
+  var newList []Item
+
+  for _, item := range list {
+    if fn(item) {
+      newList = append(newList, item)
+    }
+  }
+
+  return newList
+}
+
+func Map[Input interface{}, Output interface{}](list []Input, fn func(Input)Output) []Output {
+  var newList []Output
+
+  for _, item := range list {
+    newList = append(newList, fn(item))
+  }
+
+  return newList
+}
