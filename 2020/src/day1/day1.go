9af49c9e6e0732532edcc4cc7e76da1752d37c78
Dorian Amouroux <dor.amouroux@gmail.com>
2022-09-02 18:15:53 +0200

    clear split between day 1 and day 2

--- 2020/src/day1/day1.go
+++ 2020/src/day1/day1.go
@@ -34,6 +34,18 @@ func atoiSlice(lines []string) ([]int, error) {
 
 func part1(lines []int) int {
   nbLines := len(lines)
+  for i := 0; i < nbLines - 1; i++ {
+    for j := i + 1; j < nbLines; j++ {
+      if lines[i] + lines[j] == 2020 {
+        return lines[i] * lines[j]
+      }
+    }
+  }
+  return -1
+}
+
+func part2(lines []int) int {
+  nbLines := len(lines)
   for i := 0; i < nbLines - 2; i++ {
     for j := i + 1; j < nbLines - 1; j++ {
       for k := j + 1; k < nbLines; k++ {
@@ -56,4 +68,5 @@ func main() {
     fmt.Println(errFile)
   }
   fmt.Println("part1 = ", part1(data))
+  fmt.Println("part2 = ", part2(data))
 }
