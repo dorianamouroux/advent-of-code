a63e787bcc17977e46ca953a4397d54d16e3932b
Dorian Amouroux <dor.amouroux@gmail.com>
2022-09-06 23:56:25 +0200

    day 8

---
+++ 2020/src/day8/day8.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+
+  "github.com/dorianamouroux/advent-of-code/src/utils"
+)
+
+type instruction struct {
+  name string
+  value int
+  wasRan bool
+}
+
+// return the accumulator value, and boolean to indicate if it had infinite loop
+func runProgram(instructions []*instruction) (int, bool) {
+  currentLine, accumulator := 0, 0
+
+  for currentLine < len(instructions) {
+    instruction := instructions[currentLine]
+    if instruction.wasRan {
+      return accumulator, true
+    }
+    instruction.wasRan = true
+    switch instruction.name {
+    case "jmp":
+      currentLine = currentLine + instruction.value
+    case "acc":
+      accumulator = accumulator + instruction.value
+      currentLine++
+    default:
+      currentLine++
+    }
+  }
+  return accumulator, false
+}
+
+func resetProgram(instructions []*instruction) {
+  for _, instruction := range instructions {
+    instruction.wasRan = false
+  }
+}
+
+func part1(instructions []*instruction) int {
+  accumulator, _ := runProgram(instructions)
+  return accumulator
+}
+
+func part2(instructions []*instruction) int {
+  for _, instruction := range instructions {
+    if instruction.name == "jmp" {
+      resetProgram(instructions)
+      instruction.name = "nop"
+      accumulator, infiniteLoop := runProgram(instructions)
+      if !infiniteLoop {
+        return accumulator
+      }
+      instruction.name = "jmp"
+    }
+  }
+  return -1
+}
+
+func main() {
+  file, errFile := utils.ReadInput()
+  if errFile != nil {
+    fmt.Println(errFile)
+    return
+  }
+  instructions := utils.Map(file, func (line string)*instruction {
+    name, value, _ := strings.Cut(line, " ")
+    return &instruction{name: name, value: utils.AtoiSimple(value), wasRan: false}
+  })
+  fmt.Println("part1 =", part1(instructions))
+  fmt.Println("part2 =", part2(instructions))
+}
